583fc97e6b0a2997b2f97855981710a08da31e11
deshinta <deshintaaulia065@gmail.com>
2024-07-31 14:35:00 +0700

    feat: add input form after styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,43 @@ import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState("");
+  const [endDate, setEndDate] = useState(new Date());
   const [popUpDate, setPopUpDate] = useState(false);
   const onChange = (dates) => {
     const [start, end] = dates;
     setStartDate(start);
     setEndDate(end);
   };
-  console.log({ startDate, endDate }, "<<< date");
-  console.log(typeof startDate, "<<< type of");
+  // console.log({ startDate, endDate }, "<<< date");
+  // console.log(
+  //   `${startDate.getDate()}-${startDate.getMonth()}-${startDate.getFullYear()}`,
+  //   "<<< type of"
+  // );
+
+  const formatDate = (date) =>
+    `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+
   return (
     <>
-      <form action="">
+      <form action="" className="grid justify-center items-center">
         {/* <label htmlFor="date-range" onClick={() => setPopUpDate(true)}>
-          start date{startDate} end date {endDate}
+          start date {formatDate(startDate)} end date {formatDate(endDate)}
         </label> */}
-        <DatePicker
-          selected={startDate}
-          onChange={onChange}
-          startDate={startDate}
-          endDate={endDate}
-          selectsRange
-          inline
-        />
+        <div className="grid">
+          {/* <label htmlFor="date">
+            {formatDate(startDate)} to {formatDate(endDate)}
+          </label> */}
+          <DatePicker
+            className="p-3 w-64"
+            selectsRange={true}
+            startDate={startDate}
+            endDate={endDate}
+            onChange={onChange}
+            isClearable={false}
+            dateFormat="d MMMM yyyy"
+            placeholderText="Date Book"
+          />
+        </div>
         {/* {!popUpDate ? (
           ""
         ) : (
