0b0d8e02a21f4c60949b3a035b996718bbe7b8b6
deshinta <deshintaaulia065@gmail.com>
2024-07-31 16:11:05 +0700

    style: style ring dropdown

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -21,7 +21,7 @@ const DropDown = ({ options, selectedOptions, setSelectedOptions }) => {
       <div>
         <button
           type="button"
-          className="rounded-full text-black font-bold p-2 focus:outline-none focus:ring-2 ring-[#D2AD83] w-64"
+          className="rounded-full text-black font-bold p-2 outline-none ring-2 ring-[#D2AD83] w-64"
           id="options-menu"
           onClick={toggleDropdown}
         >
@@ -30,26 +30,24 @@ const DropDown = ({ options, selectedOptions, setSelectedOptions }) => {
       </div>
 
       {isOpen && (
-        <div className="">
-          <div className="py-1">
-            {options.map((option) => (
-              <div key={option} className="flex items-center px-4 py-2">
-                <input
-                  id={`checkbox-${option}`}
-                  type="checkbox"
-                  className="h-4 w-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
-                  checked={selectedOptions.includes(option)}
-                  onChange={() => handleCheckboxChange(option)}
-                />
-                <label
-                  htmlFor={`checkbox-${option}`}
-                  className="ml-2 block text-sm text-gray-700"
-                >
-                  {option}
-                </label>
-              </div>
-            ))}
-          </div>
+        <div className="py-1">
+          {options.map((option) => (
+            <div key={option} className="flex items-center px-4 py-2">
+              <input
+                id={`checkbox-${option}`}
+                type="checkbox"
+                className="h-4 w-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
+                checked={selectedOptions.includes(option)}
+                onChange={() => handleCheckboxChange(option)}
+              />
+              <label
+                htmlFor={`checkbox-${option}`}
+                className="ml-2 block text-sm text-gray-700"
+              >
+                {option}
+              </label>
+            </div>
+          ))}
         </div>
       )}
     </div>
